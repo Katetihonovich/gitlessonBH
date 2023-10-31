5c1a4e288c39d373537563911c41a6e2b155b831
Ekaterina Tihonovich <katetihonovich@gmail.com>
2023-10-31 22:34:08 +0100

    add input function

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 print("Hello github!")
 print("Hello World!")
-a=5
+a = 5
 print(a)
 print("Welcome everyone! " * 5)
 print("Hello again " * 3)
-
-
-
+name = input("Enter name: ")
+print("Hello " + name)
