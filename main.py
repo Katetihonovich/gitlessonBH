f3baa7f68d54509460b52d7ecfc346ee7c8956d5
Ekaterina Tihonovich <katetihonovich@gmail.com>
2023-11-01 10:31:39 +0100

    add whitespaces

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 print("Hello github!")
 print("Hello World!")
-a=5
+a = 5
 print(a)
 print("Welcome everyone! " * 5)
 print("Hello again " * 3)
 name = input("whats your name?  ")
 print("Welcome " + name)
-
-
-
-
